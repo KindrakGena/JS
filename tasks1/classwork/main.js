a989bfc2969725a523940f5ad71e4358e2f29d0d
KindrakGena <kindrak1985@gmail.com>
2022-02-02 18:10:15 +0200

    Initial commit

---
+++ tasks1/classwork/main.js
@@ -0,0 +1,85 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+
+// let num = [40, 35, 20, Math.floor(Math.random() * 10), Math.floor(7, 77), 10,
+//     Math.floor(Math.random() * 10), 5, 1, Math.floor(15.5)]
+// console.log(num);
+
+// let result = num[0] + num[1] + num[2] + num[3] + num[4] + num[5] + num[6] + num[7] + num[8] + num[9];
+// console.log(result);
+// let result = 0;
+// for (let i = 0; i < num.length; i++) {
+//     console.log(result+=num[i]);
+// }
+
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+// let book = {
+//     title : 'Institute',
+//     pages : 400,
+//     gener : 'novel'
+// }
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+//  let book = {
+//     title : 'Institute',
+//     pages : 400,
+//     gener : 'novel',
+//      author : "Stiven King"
+//  }
+// console.log(book);
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+
+// let book1 = {
+//     title : 'Institute',
+//      pages : 400,
+//      gener : 'novel',
+//      author : "Stiven King"
+// };
+// let book2 = {
+//     title : 'HarryPotter',
+//     pages : 300,
+//     gener : 'novel',
+//     author : "Rowling"
+// };
+// let book3 = {
+//     title : 'TheWitcher',
+//     pages : 300,
+//     gener : 'novel',
+//     author : "SomeAuthor"
+// };
+// let book4 = {
+//     title : 'Kobzar',
+//     pages : 500,
+//     gener : 'novel',
+//     author : "Shevchenko"
+// };
+//
+// let arrBook = [book1, book2, book3, book4];
+// console.log(arrBook);
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+
+// let height = 23;
+// let width = 10;
+// let s = height * width;
+// console.log(s);
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// let pi = 3.14;
+// let heightC = 10;
+// let dC = 4;
+// let v = pi / 4 * Math.pow(4, 2) * 10;
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь *
+
+ // let n = 3;
+ // let m = 4;
+ // let k = Math.sqrt(Math.pow(3,2)+Math.pow(4,2));
+ // console.log(k);
---
+++ tasks1/homework/main.js
@@ -0,0 +1,101 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello = 'hello';
+// let owa = 'owa';
+// let com = 'com';
+// let ua = 'ua';
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let pi = 3.14;
+// let num5 = 2.7;
+// let num6 = 16;
+// let bul1 = true;
+// let bul2 = false;
+//
+// console.log(hello);
+// console.log(owa);
+// console.log(com);
+// console.log(ua);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(pi);
+// console.log(num5)
+// console.log(num6);
+// console.log(bul1);
+// console.log(bul2);
+//
+// document.write(`<h3>${hello}</h3>`);
+// document.write(`<h3>${owa}.${com}.${ua}</h3>`);
+// document.write(`<h3>${num1}, ${num2}, ${num3}, ${num4}, ${num5}, ${num6}, ${pi}</h3>`);
+// document.write(`<h3>${bul1} and ${bul2}</h3>`);
+// alert(`${hello} world`);
+// alert(`${owa}.${com}.${ua}`);
+// alert(`${num1}, ${num2}, ${num3}, ${num4}, ${num5}, ${num6}, ${pi}`);
+// alert(`${bul1} and ${bul2}`);
+
+// Переприсвоїти кожній змінній з завдання значення на довільне.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// hello = 'world';
+// owa = 'google';
+// com = 'ua';
+// ua = 'com';
+// num1 = 3;
+// num2 = 15;
+// num3 = 999;
+// num4 = -123;
+// pi = 34;
+// num5 = 7.2;
+// num6 = 162;
+// bul1 = false;
+// bul2 = true;
+//
+// console.log(hello);
+// console.log(owa);
+// console.log(com);
+// console.log(ua);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(pi);
+// console.log(num5)
+// console.log(num6);
+// console.log(bul1);
+// console.log(bul2);
+//
+// document.write(`<h3>${hello}</h3>`);
+// document.write(`<h3>${owa}.${com}.${ua}</h3>`);
+// document.write(`<h3>${num1}, ${num2}, ${num3}, ${num4}, ${num5}, ${num6}, ${pi}</h3>`);
+// document.write(`<h3>${bul1} and ${bul2}</h3>`);
+// alert(`${hello} world`);
+// alert(`${owa}.${com}.${ua}`);
+// alert(`${num1}, ${num2}, ${num3}, ${num4}, ${num5}, ${num6}, ${pi}`);
+// alert(`${bul1} and ${bul2}`);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+// let firstName = 'Kindrak';
+// let middleName = 'Evheniy';
+// let lastName = 'Muronovich';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person);
+// document.write(`<h2>${firstName} ${middleName} ${lastName}</h2>`);
+
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let name = prompt('імя');
+// let surname = prompt('По батькові');
+// let age = prompt('Вік');
+//
+// alert(`Вітаю ${name} ${surname}. Тобі ${age} років.`)
+
+
+
