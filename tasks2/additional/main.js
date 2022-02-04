8fb0ce095d809cf9c9d2946bc1b54970b10eb877
KindrakGena <kindrak1985@gmail.com>
2022-02-04 13:12:58 +0200

    classwork end

---
+++ tasks2/additional/main.js
@@ -0,0 +1,13 @@
+// let a = 1;
+// let b = 5
+// let result;
+// if (a + b < 4) {
+//     result = 'Мало';
+//     console.log(result)
+// } else {
+//     result = 'Багато';
+//     console.log(result)
+// }
+//
+// let result = (a + b < 4) ? 'Мало': 'Багато';
+// console.log(result)
