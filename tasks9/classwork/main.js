533161572716a1139f19ae4adf4318350c22e5d0
KindrakGena <kindrak1985@gmail.com>
2022-03-10 19:08:37 +0200

    lesson 8 check

--- tasks9/classwork/main.js
+++ tasks9/classwork/main.js
@@ -85,99 +85,100 @@
 // }
 
 // Є масив
-// let coursesArray = [
-//     {
-//         title: 'JavaScript Complex',
-//         monthDuration: 5,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
-//     },
-//     {
-//         title: 'Java Complex',
-//         monthDuration: 6,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'git',
-//             'java core',
-//             'java advanced']
-//     },
-//     {
-//         title: 'Python Complex',
-//         monthDuration: 6,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'python core',
-//             'python advanced']
-//     },
-//     {
-//         title: 'QA Complex',
-//         monthDuration: 4,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
-//     },
-//     {
-//         title: 'FullStack',
-//         monthDuration: 7,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'react',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'git',
-//             'node.js',
-//             'python',
-//             'java']
-//     },
-//     {
-//         title: 'Frontend',
-//         monthDuration: 4,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
-//     }
-// ];
-// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
-// Приклад структири знаходиться у файлі example.png
-//
-//  for (let coursesArrayElement of coursesArray) {
-//
-//      let course = document.createElement('div');
-//
-//      let cursesTitle = document.createElement('div');
-//      cursesTitle.innerText = coursesArrayElement.title;
-//
-//      let cursesDuration = document.createElement('div');
-//      cursesDuration.innerText = coursesArrayElement.monthDuration;
-//
-//      let cursesHour = document.createElement('div');
-//      cursesHour.innerText = coursesArrayElement.hourDuration;
-//
-//      let discrModules = document.createElement('ul');
-//      for (let discrModule of discrModules) {
-//          let moduleCourse = document.createElement('li');
-//          moduleCourse.innerText = discrModule;
-//          discrModules.append(moduleCourse);
-//      }
-//
-//     course.append(cursesTitle, cursesDuration, cursesHour, discrModules);
-//      document.body.append(course);
-//  }
+ let coursesArray = [
+     {
+         title: 'JavaScript Complex',
+         monthDuration: 5,
+         hourDuration: 909,
+         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+     },
+     {
+         title: 'Java Complex',
+         monthDuration: 6,
+         hourDuration: 909,
+         modules: ['html',
+             'css',
+             'js',
+             'mysql',
+             'mongodb',
+             'angular',
+             'aws',
+             'docker',
+             'git',
+             'java core',
+             'java advanced']
+     },
+     {
+         title: 'Python Complex',
+         monthDuration: 6,
+         hourDuration: 909,
+         modules: ['html',
+             'css',
+             'js',
+             'mysql',
+             'mongodb',
+             'angular',
+             'aws',
+             'docker',
+             'python core',
+             'python advanced']
+     },
+     {
+         title: 'QA Complex',
+         monthDuration: 4,
+         hourDuration: 909,
+         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+     },
+     {
+         title: 'FullStack',
+         monthDuration: 7,
+         hourDuration: 909,
+         modules: ['html',
+             'css',
+             'js',
+             'mysql',
+             'mongodb',
+             'react',
+             'angular',
+             'aws',
+             'docker',
+             'git',
+             'node.js',
+             'python',
+             'java']
+     },
+     {
+         title: 'Frontend',
+         monthDuration: 4,
+         hourDuration: 909,
+         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+     }
+ ];
+ // Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+ // Приклад структири знаходиться у файлі example.png
+
+  for (let coursesArrayElement of coursesArray) {
+
+      let course = document.createElement('div');
+
+      let cursesTitle = document.createElement('div');
+      cursesTitle.innerText = coursesArrayElement.title;
+
+      let cursesDuration = document.createElement('div');
+      cursesDuration.innerText = coursesArrayElement.monthDuration;
+
+      let cursesHour = document.createElement('div');
+      cursesHour.innerText = coursesArrayElement.hourDuration;
+
+      let discrModules = document.createElement('ul');
+
+      for (let discrModule of coursesArrayElement.modules ) {
+          let moduleCourse = document.createElement('li');
+          moduleCourse.innerText = discrModule;
+          discrModules.append(moduleCourse);
+      }
+
+     course.append(cursesTitle, cursesDuration, cursesHour, discrModules);
+      document.body.append(course);
+  }
 
